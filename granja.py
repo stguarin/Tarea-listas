72ac48bff32fe8abc4692148a52d4c596788f0df
Arnold Merchan <arnoldmerchan2005@gmail.com>
2024-11-27 20:58:57 -0500

    egunda_parte

--- granja.py
+++ granja.py
@@ -11,9 +11,11 @@ x = int(input('ingrese la cantidad de metros cuadrados que pastan las vacas en u
 litros = v*(k*x)
 #se imprime el resultado
 print(f'la cantidad de litros de leche producidos por dia en la granja es de: {litros}\nla cantidad de litros de leche que produce la granja en una semana es de: {litros*7}')
-#nueva variable x
+#nueva variable cant_gallinas donde se calcula la cantidad de gallinas excluyendo a los pollos
 cant_galli = (a//3)//2
+#se calcula la cantiddad de huevos producidos
 huev_cad3= cant_galli*10
 huev_cad5=cant_galli*6
+#se halla el total de huevos producidos y se imprime
 tot=huev_cad3+huev_cad5
 print(f'la cantidad aproximada de huevos producidos en un mes es de {tot}')
